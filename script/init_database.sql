dadaa8c4dbb5a8190c339e7c8eb15a0029283935
Suraj Chaurasiya <36192974+surajchaurasiya@users.noreply.github.com>
2026-08-05 19:21:52 +0530

    Update init_database.sql

--- script/init_database.sql
+++ script/init_database.sql
@@ -1,1 +1,26 @@
+--Create Database Datawarehouse
+use master;
+Go
 
+--Drop and recreate the Datawarehouse database.
+if exists(select 1 from sys.databases where name = 'DataWarehouse')
+Begin
+	alter database DataWarehouse set single_user with Rollback immediate;
+	Drop Database DataWarehouse;
+End;
+Go
+
+--create the Datawarehouse database.
+Create Database DataWarehouse;
+Go
+
+use DataWarehouse;
+
+--Create Schema
+Create Schema bronze;
+Go
+
+Create Schema silver;
+Go
+
+Create Schema gold;
